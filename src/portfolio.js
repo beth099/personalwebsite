3af2b86b56180f547eb84d7558fe7f8ef420e44f
Beth Clarke <86461495+beth099@users.noreply.github.com>
2026-08-01 13:12:13 +0200

    fix second lottie

--- src/portfolio.js
+++ src/portfolio.js
@@ -14,7 +14,7 @@ const splashScreen = {
 
 // Summary And Greeting Section
 const illustration = {
-  animated: false // Set to false to use static SVG
+  animated: true // Set to false to use static SVG
 };
 
 const greeting = {
